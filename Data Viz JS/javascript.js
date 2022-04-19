ae6ff9c03ea92962c293bc54bdcc4550eb3515eb
maddieos <mosgood@terpmail.umd.edu>
2022-04-19 18:49:34 -0400

    final

--- Data Viz JS/javascript.js
+++ Data Viz JS/javascript.js
@@ -8,10 +8,10 @@ var myChart = new Chart(ctx, {
             label: '# of hate crimes',
             data: [8219, 9730, 14970, 22635, 30740, 37055, 46290, 53375, 64312, 59517, 66330, 69289, 79128, 78572, 78386, 88065, 100416, 124457, 129078, 149948, 195900],
             fill: true,
-            borderColor: 'rgba(0, 156, 59)',
-            backgroundColor: 'rgba(0, 156, 59, 0.1)',
+            borderColor: 'rgba(153, 0, 0)',
+            backgroundColor: 'rgba(153, 0, 0, 0.1)',
             tension: 0.1},
-          },
+          ]},
 
 
     options: {
@@ -19,14 +19,14 @@ var myChart = new Chart(ctx, {
                 plugins: {
                     title: {
                         display: true,
-                        text: 'The United States has had the highest life expectancy since 1800',
+                        text: 'Hate crimes in the US spiked by 30.6% from 2019 to 2020',
                         font: {
                             size: 18
                         }
                     },
                     subtitle: {
                         display: true,
-                        text: 'According to Gapminder, the United States has had the highest life expectancy of the largest countries in the world, while India has had the lowest since 1800.'
+                        text: 'According to FBI UCR hate crime data, this is the largest yearly increase in hate crimes since 2000. The US has experienced a relatively steady increase in hate crime incidients over the past 20 years. '
                     }
                 },
                 scales: {
